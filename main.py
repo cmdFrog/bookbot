4bc9c87d72c90b01a7b7a6276db13798c8651234
cmdFrog <cmdfroggit@gmail.com>
2024-05-05 18:28:57 -0700

    now returns word count along with a dictionary of letter counts

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     file_contents = get_book_text(book_path)
     num_words = word_count(file_contents)
-    print(f"words found in document; {num_words}")
+    letter_count = count_letters(file_contents)
+    #print(f"Words found in document: {num_words}. Letter counts: {letter_count}")
+    
+    print(f"word count: {num_words}, character dictionary: {letter_count}")
+    
+    
+    
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,6 +17,22 @@ def get_book_text(path):
 def word_count(string):
     words = string.split()
     return len(words)
+
+def count_letters(string):
+    lowered_string = string.lower()
+    string_words = list(lowered_string)
+    wordcount_dictionary = {}
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for b in letters:
+        letterCounter = 0
+        for i in range(0, len(string_words)):
+            if b == string_words[i]:
+                letterCounter += 1
+        wordcount_dictionary[b] = letterCounter
+        
+    return wordcount_dictionary
+
+
 
 
 
